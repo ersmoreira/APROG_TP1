44e981406ef61704d464dbefdac25113fc892587
Edgar Moreira <ersmoreira@gmail.com>
2021-12-08 19:29:51 +0000

    Utilização de ficheiro para dados de input

--- src/TrabalhoPratico.java
+++ src/TrabalhoPratico.java
@@ -1,49 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TrabalhoPratico {
 
-    final static int DIM_VERTICAL_INDEX = 0;
-    final static int DIM_HORIZONTAL_INDEX = 1;
+    final static int DIM_HORIZONTAL_INDEX = 0;
+    final static int DIM_VERTICAL_INDEX = 1;
     final static int ALTERACAO_NIVEL_AGUA = -1;
-    final static Scanner sc = new Scanner(System.in);
+    final static int QUANTIDADE_DIMENSOES = 2;
 
-    public static void main(String[] args) {
+    final static String FILE_NAME = "./src/data.txt";
+    final static String FILE_DELIMITER = " ";
 
-        System.out.println("a)");
+    static Scanner sc_file;
+    static int dimensaoVertical, dimensaoHorizontal;
+    static char alinea = 'a';
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        sc_file = new Scanner(new File(FILE_NAME));
 
         // Descritivo do terreno
-        String descritivoTerreno = lerString(sc);
+        String descritivoTerreno = sc_file.nextLine();
 
         // Dimensoes horizontal e vertical
-        String[] arrayDimensoes = obterDimensoesTerreno();
-        int dimensaoVertical = Integer.parseInt(arrayDimensoes[DIM_VERTICAL_INDEX]);
-        int dimensaoHorizontal = Integer.parseInt(arrayDimensoes[DIM_HORIZONTAL_INDEX]);
+        int[] arrayDimensoes = obterDimensoesTerreno();
+        dimensaoHorizontal = arrayDimensoes[DIM_HORIZONTAL_INDEX];
+        dimensaoVertical = arrayDimensoes[DIM_VERTICAL_INDEX];
 
         // Obter cotas do terreno
-        int[][] terreno = obterCotasTerreno(dimensaoVertical, dimensaoHorizontal);
+        int[][] terreno = obterCotasTerreno();
 
         // Imprime o mapa do terreno
-        System.out.println("b)");
-        imprimeMapaTerreno(terreno, dimensaoVertical, dimensaoHorizontal);
+        imprimeMapaTerreno(terreno);
 
         // Obter e mostrar novo mapa com alteracao do nivel da agua
-        int[][] mapaAlterado = calculaNovoMapaTerreno(terreno, dimensaoVertical, dimensaoHorizontal);
-        System.out.println("c)");
-        imprimeMapaTerreno(mapaAlterado, dimensaoVertical, dimensaoHorizontal);
+        int[][] mapaAlterado = calculaNovoMapaTerreno(terreno);
+        imprimeMapaTerreno(mapaAlterado);
 
         // Mostrar percentagem de area do terreno submersa
-        System.out.println("d)");
-        mostraPercentagemTerrenoSubmerso(calculaPercentagemTerrenoSubmerso(mapaAlterado, dimensaoVertical,
-                dimensaoHorizontal));
+        mostraPercentagemTerrenoSubmerso(calculaPercentagemTerrenoSubmerso(mapaAlterado));
 
     }
 
     // Calcula novo mapa do terreno tendo em conta a alteração do nivel da agua
-    private static int[][] calculaNovoMapaTerreno(int[][] mapaTerreno, int dimensaoLinhas, int dimensaoColunas) {
-        int[][] novoMapa = new int[dimensaoLinhas][dimensaoColunas];
+    private static int[][] calculaNovoMapaTerreno(int[][] mapaTerreno) {
+        int[][] novoMapa = new int[dimensaoHorizontal][dimensaoVertical];
 
-        for (int linha = 0; linha < dimensaoLinhas; linha++) {
-            for (int coluna = 0; coluna < dimensaoColunas; coluna++) {
+        for (int linha = 0; linha < dimensaoHorizontal; linha++) {
+            for (int coluna = 0; coluna < dimensaoVertical; coluna++) {
                 novoMapa[linha][coluna] = mapaTerreno[linha][coluna] - ALTERACAO_NIVEL_AGUA;
             }
 
@@ -53,53 +58,45 @@ public class TrabalhoPratico {
     }
 
     // Obtem as cotas de cada linha do terreno
-    private static int[][] obterCotasTerreno(int dimensaoLinhas, int dimensaoColunas) {
-        int[][] cotasTerreno = new int[dimensaoLinhas][dimensaoColunas];
+    private static int[][] obterCotasTerreno() {
+        int[][] cotasTerreno = new int[dimensaoHorizontal][dimensaoVertical];
+        int numeroLinha = 0;
+        String[] arrayAux;
         String cotasLinha;
 
-        for (int linha = 0; linha < dimensaoLinhas; linha++) {
-            cotasLinha = lerString(sc);
-            cotasTerreno[linha] = stringToArrayIntBySpace(cotasLinha);
+        while (sc_file.hasNextLine()) {
+            cotasLinha = sc_file.nextLine();
+            arrayAux = cotasLinha.split(FILE_DELIMITER);
+
+            for (int i = 0; i < arrayAux.length; i++) {
+                cotasTerreno[numeroLinha][i] = Integer.parseInt(arrayAux[i]);
+            }
+
+            numeroLinha++;
         }
 
         return cotasTerreno;
     }
 
     // Obtem as dimensoes do terreno
-    public static String[] obterDimensoesTerreno() {
-        String dimensoes = lerString(sc);
-        String[] arrayDimensoes = stringToArrayBySpace(dimensoes);
-        return arrayDimensoes;
-    }
+    public static int[] obterDimensoesTerreno() {
+        String[] arrString = sc_file.nextLine().split(FILE_DELIMITER);
+        int[] arrInt = new int[QUANTIDADE_DIMENSOES];
 
-    // Método que lê e retorna uma string
-    public static String lerString(Scanner sc) {
-        String texto;
-        texto = sc.nextLine();
-        return texto.trim();
-    }
-
-    // Metodo que converte e retorna uma String num Array de Strings dividindo pelo espaço
-    public static String[] stringToArrayBySpace(String texto) {
-        String[] array = texto.split(" ");
-        return array;
-    }
-
-    // Metodo que converte e retorna uma String num Array de inteiros dividindo pelo espaço
-    public static int[] stringToArrayIntBySpace(String texto) {
-        String[] arrayString = stringToArrayBySpace(texto);
-        int[] arrayInt = new int[arrayString.length];
-
-        for (int i = 0; i < arrayString.length; i++) {
-            arrayInt[i] = Integer.parseInt(arrayString[i]);
+        for (int i = 0; i < QUANTIDADE_DIMENSOES; i++) {
+            arrInt[i] = Integer.parseInt(arrString[i]);
         }
-        return arrayInt;
+
+        return arrInt;
     }
+
 
     // Imprime o mapa do terreno com com os valores alinhados à direita
-    public static void imprimeMapaTerreno(int[][] mapaTerreno, int dimensaoLinhas, int dimensaoColunas) {
-        for (int linha = 0; linha < dimensaoLinhas; linha++) {
-            for (int coluna = 0; coluna < dimensaoColunas; coluna++) {
+    public static void imprimeMapaTerreno(int[][] mapaTerreno) {
+        mostraProximaAlinea();
+
+        for (int linha = 0; linha < dimensaoHorizontal; linha++) {
+            for (int coluna = 0; coluna < dimensaoVertical; coluna++) {
                 System.out.printf("%5d", mapaTerreno[linha][coluna]);
             }
             System.out.println();
@@ -107,21 +104,26 @@ public class TrabalhoPratico {
     }
 
     // Calcula percentagem de terreno submersa
-    public static float calculaPercentagemTerrenoSubmerso(int[][] terreno, int dimensaoLinhas, int dimensaoColunas) {
+    public static float calculaPercentagemTerrenoSubmerso(int[][] terreno) {
         int quantidade = 0;
-        for (int linha = 0; linha < dimensaoLinhas; linha++) {
-            for (int coluna = 0; coluna < dimensaoColunas; coluna++) {
+        for (int linha = 0; linha < dimensaoHorizontal; linha++) {
+            for (int coluna = 0; coluna < dimensaoVertical; coluna++) {
                 if (terreno[linha][coluna] < 0) {
                     quantidade++;
                 }
             }
         }
-        return ((float) quantidade / (dimensaoLinhas * dimensaoColunas)) * 100;
+        return ((float) quantidade / (dimensaoHorizontal * dimensaoVertical)) * 100;
     }
 
     // Mostra área submersa
     public static void mostraPercentagemTerrenoSubmerso(float valor) {
+        mostraProximaAlinea();
         System.out.printf("area submersa: %7.2f%%", valor);
     }
 
+    public static void mostraProximaAlinea() {
+        alinea++;
+        System.out.printf("%c)\n", alinea);
+    }
 }
